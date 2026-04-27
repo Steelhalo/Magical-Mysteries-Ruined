4178f44b3f2716dc665899f3a1e790bb3030ed3b
Steelhalo <Dukeevan806@gmail.com>
2026-04-27 09:37:08 +0100

    Delete kubejs directory
    Moving to 1.21.1 neoforge

--- kubejs/server_scripts/recipies/magical_addons/magical_addons_furnace_recipies.js
+++
@@ -1,4 +0,0 @@
-//Furnace Smelting
-ServerEvents.recipes(event => {
-    event.smelting('bewitchment:unpowered_silver_ingot', 'bewitchment:raw_silver').xp(0.35)
-  })
--- kubejs/startup_scripts/block_modification.js
+++
@@ -1,4 +0,0 @@
-BlockEvents.modification(event => {
-    event.modify('bewitchment:silver_block', block => {
-    })
-})
